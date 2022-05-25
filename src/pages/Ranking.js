042668fc2ee268397080c315b88bf41045060415
Bruna Luchini <brunaluchini22@gmail.com>
2022-05-25 18:26:49 -0300

    feat: add button go back

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Ranking extends Component {
   render() {
+    const { history } = this.props;
     return (
       <div>
         <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          type="button"
+          onClick={ () => history.push('/') }
+          data-testid="btn-go-home"
+        >
+          Voltar para a tela inicial
+        </button>
       </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.objectOf(PropTypes.shape).isRequired,
+};

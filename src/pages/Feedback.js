cc9a68fe77f78a15e4de06b91d24b4db86edd255
Rodrigo Takashi Condo <rodrigo0takashi@gmail.com>
2022-05-25 16:41:53 -0300

    [GROUP 23] TRIVIA - Requisito 16

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 
 export default class Feedback extends Component {
   render() {
+    const { history } = this.props;
     return (
       <div>
         <h1 data-testid="feedback-text">
           Feedback
         </h1>
+        <button
+          data-testid="btn-ranking"
+          type="button"
+          onClick={ () => history.push('/ranking') }
+        >
+          Ranking
+        </button>
       </div>
     );
   }

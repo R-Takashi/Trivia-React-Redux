2fdae2496bda4c02d7d278403244da3f11137957
Bruna Luchini <brunaluchini22@gmail.com>
2022-05-25 22:29:38 -0300

    chore: arrumando identação do initial_state

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -38,19 +38,19 @@ describe('Testando o componente Login', () => {
       questions: {
         response_code: 0,
         results:[
-           {
-              category: "Entertainment: Video Games",
-              type: "multiple",
-              difficulty: "easy",
-              question: "What is the first weapon you acquire in Half-Life?",
-              correct_answer: "A crowbar",
-              incorrect_answers: [
-                 "A pistol",
-                 "The H.E.V suit",
-                 "Your fists"
-              ]
-           }
-        ]
+          {
+            category: "Entertainment: Video Games",
+            type: "multiple",
+            difficulty: "easy",
+            question: "What is the first weapon you acquire in Half-Life?",
+            correct_answer: "A crowbar",
+            incorrect_answers: [
+              "A pistol",
+              "The H.E.V suit",
+              "Your fists"
+            ],
+          },
+        ],
       }
     };
 
